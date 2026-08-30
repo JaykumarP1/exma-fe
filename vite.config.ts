dbb8cdc54678430cb936dbb764948f00a2c4d42a
Jaykumarp1 <jaykumarp1559@gmail.com>
2026-08-30 21:47:56 +0530

    Configure Vite to use port 4001

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 4001,
+    strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:4000',
         changeOrigin: true,
         secure: false,
       }
